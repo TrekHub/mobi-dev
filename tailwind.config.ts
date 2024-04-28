503e135531b287b310c5dfede2fa7aaaa5ed4801
Trekhub <Teddyomondi001@gmail.com>
2024-04-28 13:29:46 +0300

    Add Flowbite and Flowbite-react dependencies, update tailwind.config.ts, and modify app/layout.tsx

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./node_modules/flowbite/src/**/*.{js,ts,jsx,tsx,mdx}",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -13,8 +14,15 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        nav: "#070607",
+        "footer-bg": "#1d1d1c",
+        "yellow-accent": "#ffe047",
+        "green-accent": "#00ab8d",
+        "purple-accent": "#4b3eb6",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("flowbite/plugin")],
 };
 export default config;
