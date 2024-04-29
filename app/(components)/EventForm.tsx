9187fa80e5ff01468690b828349a43a307fe8a06
Trekhub <Teddyomondi001@gmail.com>
2024-04-29 22:08:32 +0300

    Update EventForm component to include file upload functionality and handle file changes

--- app/(components)/EventForm.tsx
+++ app/(components)/EventForm.tsx
@@ -1,7 +1,8 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 
 
-import React, { ChangeEvent, useState } from 'react'
+import React, { ChangeEvent, useRef, useState } from 'react'
 import Banner from './Banner';
 import { useRouter } from 'next/navigation';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -12,6 +13,24 @@ import Link from 'next/link';
 const EventForm = ({ event }: any) => {
     const EDITMODE = event._id === "new" ? false : true;
     const router = useRouter();
+    const fileRef = useRef<HTMLInputElement>(null);
+
+    const handleBtnClick = () => {
+        fileRef.current?.click();
+    }
+
+    const [file, setFile] = useState<any>(null);
+    const [fileName, setFileName] = useState<string>("" || event.banner_img);
+
+    const handleChangFile = (e: ChangeEvent<HTMLInputElement>) => {
+        console.log("clicked");
+        if (e.target.files) {
+            setFile(e.target.files[0]);
+        }
+    };
+
+
+
 
 
     const initialState = {
@@ -46,6 +65,8 @@ const EventForm = ({ event }: any) => {
 
     const [formData, setFormData] = useState(initialState);
 
+
+
     const handleChange = (
         e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
     ) => {
@@ -59,6 +80,7 @@ const EventForm = ({ event }: any) => {
     const handleSubmit = async (e: any) => {
         e.preventDefault();
 
+        // console.log(file)
 
         if (EDITMODE) {
             try {
@@ -140,19 +162,33 @@ const EventForm = ({ event }: any) => {
 
                     <div className="mb-6">
 
+
                         <div className="flex  flex-col items-start justify-start w-full">
                             <label htmlFor="event_description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Banner Image</label>
 
-                            <label htmlFor="banner_img" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
-                                <div className="flex flex-col items-center justify-center pt-5 pb-6">
-                                    <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
-                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
-                                    </svg>
-                                    <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
-                                    <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
-                                </div>
-                                <input id="dropzone-file" type="file" className="hidden" />
-                            </label>
+                            <button type='button' className='w-full' onClick={handleBtnClick}>
+                                {/* <input id="dropzone-file" type="file" onChange={handleChangFile} className="hidden" /> */}
+                                <label htmlFor="banner_img" className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+                                    <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                                        <svg className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2" />
+                                        </svg>
+                                        <p className="mb-2 text-sm text-gray-500 dark:text-gray-400"><span className="font-semibold">Click to upload</span> or drag and drop</p>
+                                        <p className="text-xs text-gray-500 dark:text-gray-400">SVG, PNG, JPG or GIF (MAX. 800x400px)</p>
+                                    </div>
+                                    <input id="dropzone-file" ref={fileRef} type="file" onChange={({ target: { files } }) => {
+                                        files?.[0] && setFileName(files[0].name)
+                                        files && setFile(files[0])
+                                    }} className="hidden file-input" />
+
+                                    
+
+                                    {file ? <img src={URL.createObjectURL(file)} width={60}  height={60} alt="" /> : <p className="text-xs text-gray-500 dark:text-gray-400">{fileName}</p> }
+
+                                    {/* {fileName && <p className="text-xs text-gray-500 dark:text-gray-400">{fileName}</p>} */}
+                                </label>
+                            </button>
+
                         </div>
 
                     </div>
@@ -268,7 +304,7 @@ const EventForm = ({ event }: any) => {
                                     className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="" />
                             </div>
                         </div>
-                        
+
 
                     </div>
 
