9f4e4ecf07cf4a6b2da0a061ff206673e585b3f4
Trekhub <Teddyomondi001@gmail.com>
2024-04-30 04:02:47 +0300

    updated the models

--- app/page.tsx
+++ app/page.tsx
@@ -1,66 +1,58 @@
-"use cl"
 
-import { useState, useEffect } from "react";
+
 import Image from "next/image";
 import HeroSection from "./(components)/HeroSection";
 import Banner from "./(components)/Banner";
 import EventCard from "./(components)/EventCard";
+//fetching events
+const getEvents = async () => {
 
-const Home = () => {
-  const [events, setEvents] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const baseUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
-        if (!baseUrl) {
-          throw new Error("Base URL is not configured.");
-        }
-
-        const res = await fetch(`${baseUrl}/api/Events`, {
-          cache: "no-cache",
-        });
-
-        if (!res.ok) {
-          throw new Error("Failed to fetch events.");
-        }
-
-        const data = await res.json();
-        setEvents(data);
-        setLoading(false);
-      } catch (err) {
-        // console.error("An error occurred while fetching events:", err);
-        setError(null);
-        setLoading(false);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-
-  if (loading) {
-    return <div>Loading...</div>;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_API_URL;
+  console.log("baseUrl", baseUrl);
+  if (!baseUrl) {
+    return;
   }
+  try {
 
-  if (error) {
-    return <div>Error: {error}</div>;
+    const res = await fetch(`${baseUrl}/api/Events`, {
+      cache: "no-cache",
+
+    });
+
+    const data = await res.json();
+    console.log("Events:", data);
+    return data;
+  } catch (err) {
+
+    return { error: "An error occurred while fetching events" };
   }
+}
+
+const Home = async () => {
+  // if (process.env.NEXT_PUBLIC_BASE_API_URL == undefined) {
+  //   return null;
+  // }
+  const events = await getEvents();
+  // console.log(events);
 
   return (
     <div>
-      <Banner />
-      <div className="bg-cover mt-9 block w-3/4 md:w-10/12 sm:w-11/12 mx-auto">
+      <div>
+        <Banner />
+      </div>
+      <div className="bg-cover mt-9 block  w-3/4  md:w-10/12 sm:w-11/12 mx-auto">
         <HeroSection />
       </div>
-      <div className="md:w-10/12 mx-auto mt-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
-        {events.map((event: any) => (
+      <div className=" md:w-10/12 mx-auto mt-10 grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-2">
+        {events && events.map((event: any) => ( // Check if events exist
           <EventCard key={event._id} event={event} />
         ))}
+
       </div>
     </div>
   );
-};
+}
 
 export default Home;
+
+
